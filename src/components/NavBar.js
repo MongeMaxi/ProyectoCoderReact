835c771c852370baa12d7787d09b582a9baa972f
Maxi <mongemaximiliano96@gmail.com>
2022-07-26 12:25:27 -0300

    entrega desafio 2

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+function NavBar() {
+    return (
+        <header id="header">
+            <h1 className="titulo">Ireland indumentaria</h1>
+            <nav>
+                <a href="#">catalogo</a>
+                <a href="#">sobre nosotros</a>
+                <a href="#">FAQ</a>
+            </nav>
+        </header>
+    )
+}
+
+export default NavBar
